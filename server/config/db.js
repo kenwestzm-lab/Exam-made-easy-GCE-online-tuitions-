f88c06de82e65968801c832b36fc226091afea28
Peace Mindset School <kenwestzm@gmail.com>
2026-06-06 15:40:13 +0200

    Fix: Remove Render reference from db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 module.exports = async () => {
   const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error('MONGODB_URI not set in Render environment');
+  if (!uri) throw new Error('MONGODB_URI not set in environment variables');
   await mongoose.connect(uri);
   console.log('✅ MongoDB connected:', mongoose.connection.host);
 };
